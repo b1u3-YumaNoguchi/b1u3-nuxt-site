d84733b4847ce33e6003b292d7be7f0a23e4e8f6
b1u3 <bp16071@shibaura-it.ac.jp>
2019-12-27 20:08:13 +0900

    クライアント用のプロジェクト作った

---
+++ B1u3BlogClient/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+from os import path
+
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='b1u3-blog-client',
+    version='0.0.1',  # Required
+    description='ブログ書く用のクライアントプログラム群',  # Optional
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    # url='https://github.com/pypa/sampleproject',  # Optional
+    author='b1u3(Yuma Noguchi)',
+    package_dir={'': 'src'},  # Optional
+    packages=find_packages(where='src'),  # Required
+    python_requires='>=3.7',
+    install_requires=['firebase_admin'],
+    # extras_require={  # Optional
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+    # package_data={  # Optional
+    #     'sample': ['package_data.dat'],
+    # },
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    # entry_points={  # Optional
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    #},
+
+    # project_urls={  # Optional
+    # },
+)
